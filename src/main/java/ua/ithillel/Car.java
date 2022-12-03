967b1dd0a57e87a8bcb1388fc1f61bb69907074a
Andrii Markov <aricov12@gmail.com>
2022-12-03 19:52:28 +0200

    Create new gradle project

---
+++ src/main/java/ua/ithillel/Car.java
@@ -0,0 +1,13 @@
+package ua.ithillel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Car {
+    private String brand;
+    private String colour;
+    private int yearProduction;
+
+ }
---
+++ src/main/java/ua/ithillel/Main.java
@@ -0,0 +1,10 @@
+package ua.ithillel;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setBrand("Volvo");
+        System.out.println(car.getBrand());
+    }
+}
